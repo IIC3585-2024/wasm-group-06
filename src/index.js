dec924f6c81f82c47911cbd5dfd3d6fffb4b94b3
Carlos Paredes <carlos@paredesrobles.com>
2024-04-15 00:35:23 -0400

    feat(visualization): improve styling

--- src/index.js
+++ src/index.js
@@ -31,32 +31,58 @@ function runFromWasm(number) {
 
 function getAndValidateInput() {
     const input = document.getElementById('input').value;
+    const errorElement = document.getElementById('error');
 
-    if (input === '' || isNaN(input)) {
-        document.getElementById('error-text').innerHTML = 'Please enter a valid number';
+    if (input === '' || isNaN(input) || parseInt(input) < 1) {
+        errorElement.innerHTML = 'Ingresa un número válido';
         return 0;
     }
-
+    errorElement.innerHTML = '';
     return parseInt(input);
 }
 
+function buildResultsBoxes(numbers, element) {
+    element.innerHTML = '';
+
+    numbers.forEach(number => {
+        const div = document.createElement('div');
+        div.className = "flex flex-col min-w-24 h-12 px-2 py-1 items-center text-lg justify-center bg-primary rounded-lg text-white shadow-sm";
+        div.textContent = number;
+        element.appendChild(div);
+    });
+}
+
+function buildTimeBadges(time, element) {
+    element.innerHTML = '';
+
+    const div = document.createElement('div');
+    div.className = "flex flex-col min-w-24 px-2 py-1 items-center justify-center rounded-lg text-white shadow-sm bg-gray-800 rounded-lg";
+    div.textContent = `Tiempo de ejecución: ${time} ms`;
+    element.appendChild(div);
+}
+
+
 function handleSubmit() {
-    const number = parseInt((document.getElementById('input')).value);
+    const number = getAndValidateInput();
+    if (number === 0) {
+        return;
+    }
 
     const jsContentBox = document.getElementById('js-content');
     const wasmContentBox = document.getElementById('wasm-content');
 
-    jsContentBox.innerHTML = '';
-    wasmContentBox.innerHTML = '';
+    const jsTimeBox = document.getElementById('js-time');
+    const wasmTimeBox = document.getElementById('wasm-time');
+
 
     const jsResult = getTimeAndFactors(primeFactors, number);
     const wasmResult = getTimeAndFactors(runFromWasm, number);
 
+    buildTimeBadges(jsResult.runtime, jsTimeBox);
+    buildTimeBadges(wasmResult.runtime, wasmTimeBox);
 
-    jsContentBox.innerHTML += `JS: ${jsResult.factors.join(' * ')}<br>`;
-    jsContentBox.innerHTML += `Runtime: ${jsResult.runtime}ms`;
 
-    wasmContentBox.innerHTML += `C: ${wasmResult.factors.join(' * ')}<br>`;
-    wasmContentBox.innerHTML += `Runtime: ${wasmResult.runtime}ms`;
+    buildResultsBoxes(jsResult.factors, jsContentBox);
+    buildResultsBoxes(wasmResult.factors, wasmContentBox);
 
 }
